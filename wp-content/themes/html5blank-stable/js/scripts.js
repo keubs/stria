23527218114b8d282fb2c0033016486dd7a9c3cc
Kevin Cook <kevinac4@gmail.com>
2017-03-16 14:27:40 -0700

    product page styling and image selectors

--- wp-content/themes/html5blank-stable/js/scripts.js
+++ wp-content/themes/html5blank-stable/js/scripts.js
@@ -3,8 +3,33 @@
 	$(function () {
 		
 		'use strict';
-		
-		// DOM ready, take it away
+		var original_image = {
+			src: $('.woocommerce-main-image img').attr('src'),
+			srcset: $('.woocommerce-main-image img').attr('srcset')
+		};
+
+		$('.thumbnails a.zoom').bind('click', function(e){
+		  e.preventDefault();
+
+		  if(!$(this).hasClass('active')) {
+			  $('.woocommerce-main-image img').attr('src', $(this).attr('href'));
+			  $('.woocommerce-main-image img').attr('srcset', $(this).attr('href'));
+			  $('.thumbnails a.zoom').each(function(e){
+			  	$(this).removeClass('active');
+			  });
+
+
+			  $(this).addClass('active');
+		  } else {
+		  	$('.woocommerce-main-image img').attr('src', original_image.src);
+		  	$('.woocommerce-main-image img').attr('srcset', original_image.srcset);
+		  	$('.thumbnails a.zoom').each(function(e){
+		  		$(this).removeClass('active');
+		  	});
+		  }
+
+
+		});
 		
 	});
 	
